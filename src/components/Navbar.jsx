33fc28f672f11a6e88131635a5fb79969cf4eb65
AsadullahShaikh123 <asadullahshaikh713@gmail.com>
2023-09-09 19:47:54 +0500

    done with left side of Login Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,13 @@
 import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
-import { Link } from 'react-router-dom';
-function Navbar() {
-  return (
-    <Box sx={{ flexGrow: 1 }}>
+export default function Navbar() {
+  return(
+    <>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -22,13 +20,11 @@ function Navbar() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <Link to="/">FYP PORTAL</Link>
+            FYP PORTAL
           </Typography>
           <Button color="inherit" >Login</Button>
         </Toolbar>
       </AppBar>
-    </Box>
+    </>
   );
 }
-export default Navbar;
-
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,49 @@
-import React from 'react'
+import { Box, Paper } from "@mui/material";
+import React from "react";
+import xyz from '../img/MUET_Logo.png'
 
 function Login() {
   return (
-    <div>Login</div>
-  )
+    <>
+      <Box sx={{ width: "100%", height: "100%", display: "flex", m: "auto" }}>
+        <Box
+          sx={{
+            width: "50%",
+            height: "100%",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center",
+          }}
+        >
+          <Box style={{display:'flex',alignItems:'center',flexDirection:'column'}}>
+            <img src={xyz} alt="logo" width="155px" height="155px" style={{borderRadius:'100%',marginBottom:'1rem'}}/>
+            <p style={{fontSize:'28px',textAlign:'center'}}>
+              <span style={{color:'#116bcc',fontWeight:'bold'}}>MUET Portal </span>
+              for <span style={{fontWeight:'bold'}}>undergraduate</span> students
+              <br />
+              to submit their <span style={{color:'#116bcc',fontWeight:'bold'}}>thesis</span> and <span style={{color:'#116bcc',fontWeight:'bold'}}>project</span>
+            </p>
+          </Box>
+        </Box>
+        <Box
+          sx={{
+            width: "50%",
+            height: "100%",
+            border: "2px solid black",
+            display: "flex",
+            justifyContent:'center',
+            alignItems:'center'
+          }}
+        >
+          <Box style={{border:'2px solid black'}}>
+                <Paper elevation={3}>
+                    
+                </Paper>
+          </Box>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
-export default Login
+export default Login;
